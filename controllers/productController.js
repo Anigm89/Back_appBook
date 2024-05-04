123c21e42d9161428ff7c9adb71d7ad522329ca4
Aniana <anigm89@gmail.com>
2024-05-04 14:19:25 +0200

    sql

--- controllers/productController.js
+++ controllers/productController.js
@@ -154,8 +154,12 @@ const BookController = {
     async getLeidos (req, res) {
         try{
             const { uid } = req.params;
-            console.log('uid', uid)
-            const leidosquery = `SELECT b.*, u.uid, l.* FROM libros b, usuarios u, leidos l WHERE b.id = l.id_libro AND u.uid = l.uid AND u.uid = '${uid}' GROUP BY b.id, u.uid`;
+           // const leidosquery = `SELECT b.*, u.uid, l.* FROM libros b, usuarios u, leidos l WHERE b.id = l.id_libro AND u.uid = l.uid AND u.uid = '${uid}' GROUP BY b.id, u.uid`;
+            const leidosquery = `SELECT b.*, u.uid, MAX(l.id) AS leido_id, MAX(l.id_libro) AS id_libro, MAX(l.uid) AS leido_uid
+            FROM libros b
+            JOIN leidos l ON b.id = l.id_libro
+            JOIN usuarios u ON u.uid = l.uid
+            WHERE u.uid = '${uid}' GROUP BY b.id`;
             const [books] = await pool.query(leidosquery);
             res.json(books)
         }
